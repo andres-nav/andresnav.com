2076eae5c88563a363587d42d646fde6bfd1f966
Andres Navarro <contact@andresnav.com>
2023-11-21 21:36:30 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "personal website (andresnav.com)";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+  in {
+    devShells.${system}.default = with import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+      mkShell {
+        packages = [
+          nodejs
+          nodePackages.pnpm
+
+          html-tidy
+
+          hugo
+          go
+        ];
+      };
+  };
+}
